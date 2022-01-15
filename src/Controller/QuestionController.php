f2da6029e964e40d53a51aba026e38c525ea6eda
maninder1516 <maninder1516@gmail.com>
2022-01-15 19:27:43 +0530

    Twig files are added now

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,8 +4,9 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class QuestionController {
+class QuestionController extends AbstractController {
 
     /**
      * 
@@ -19,10 +20,22 @@ class QuestionController {
      * @Route("/questions/{slug}")
      */
     public function show($slug) {
-        return new Response(sprintf(
-            'Future page to show my questions "%s"!',
-            ucwords(str_replace('-', ' ', $slug))
-        ));
+
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers
+        ]);
+
+//        return new Response(sprintf(
+//            'Future page to show my questions "%s"!',
+//            ucwords(str_replace('-', ' ', $slug))
+//        ));
     }
 
 }
